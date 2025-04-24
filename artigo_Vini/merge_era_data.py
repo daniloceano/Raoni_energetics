c1c617f1e085b677cf3e1a7ef3a879cf931bcc38
daniloceano <danilo.oceano@gmail.com>
2025-04-24 19:56:46 -0300

    Results for ERA5

--- artigo_Vini/merge_era_data.py
+++ artigo_Vini/merge_era_data.py
@@ -1,17 +1,13 @@
 import xarray as xr
+from glob import glob 
+
+files = glob("era5_data_2021*.nc")
 
 # Abrir os datasets
-ds1 = xr.open_dataset("era5_data_20210625.nc")
-ds2 = xr.open_dataset("era5_data_20210626.nc")
-ds3 = xr.open_dataset("era5_data_20210627.nc")
-ds4 = xr.open_dataset("era5_data_20210628.nc")
-ds5 = xr.open_dataset("era5_data_20210629.nc")
-ds6 = xr.open_dataset("era5_data_20210630.nc")
-ds7 = xr.open_dataset("era5_data_20210701.nc")
-ds8 = xr.open_dataset("era5_data_20210702.nc")
+ds = xr.open_mfdataset(files)
 
-# Concatenar os datasets
-ds = xr.concat([ds1, ds2, ds3, ds4, ds5, ds6, ds7, ds8], dim="Time")
+# Remover coordenadas inuteis (number, expver)
+ds = ds.drop_vars(["number", "expver"])
 
 # Salvar o dataset concatenado
-ds.to_netcdf("era5_data_20210625_20210702.nc")
+ds.to_netcdf("Raoni_ERA5.nc")
